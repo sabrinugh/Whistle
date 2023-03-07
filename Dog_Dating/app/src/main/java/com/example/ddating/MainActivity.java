25035e99f9dad62f04504241b66ca0660214d96f
Lazyice1172 <chungholung14@gmail.com>
2023-03-07 15:49:30 +0000

    Test_Connection

--- Dog_Dating/app/src/main/java/com/example/ddating/MainActivity.java
+++ Dog_Dating/app/src/main/java/com/example/ddating/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Test Connection
+        // FirebaseDatabase.getInstance().getReference().child("Test").setValue("abcd");
     }
 }
