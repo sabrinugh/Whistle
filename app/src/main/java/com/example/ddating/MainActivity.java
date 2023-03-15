83b035d45b62f9b2ec56db66b16f4f8643fe2c52
Lazyice1172 <chungholung14@gmail.com>
2023-03-15 20:21:10 +0000

    Basic_Auth_Funtions

--- app/src/main/java/com/example/ddating/MainActivity.java
+++ app/src/main/java/com/example/ddating/MainActivity.java
@@ -2,20 +2,35 @@ package com.example.ddating;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test Connection
-        // FirebaseDatabase.getInstance().getReference().child("Test").setValue("abcd");
-        // Test Connection
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, startActivity.class));
+                finish();
+            }
+        });
 
     }
 }
