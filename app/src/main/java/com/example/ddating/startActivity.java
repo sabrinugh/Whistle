a5ee0b9b2904b739f6f2d53ecf24b420b6eb2957
Lazyice1172 <chungholung14@gmail.com>
2023-03-16 16:57:15 +0000

    Nothing_Change

--- app/src/main/java/com/example/ddating/startActivity.java
+++ app/src/main/java/com/example/ddating/startActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -46,6 +47,8 @@ public class startActivity extends AppCompatActivity {
 
         if(currentUser != null){
             currentUser.reload();
+            // Toast.makeText(startActivity.this, currentUser.getUid(), Toast.LENGTH_SHORT).show();
+
             startActivity(new Intent(startActivity.this, MainActivity.class));
             finish();
         }
