ed172cff9cf7dc4c2c4657cd34d8a76990cdb8e3
Milo <milek1093@gmail.com>
2023-04-02 22:44:52 +0200

    Swipe Working
    Got a basic swipe working and toast with it

--- app/src/main/java/com/example/ddating/MainActivity.java
+++ app/src/main/java/com/example/ddating/MainActivity.java
@@ -1,21 +1,72 @@
 package com.example.ddating;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.daprlabs.cardstack.SwipeDeck;
 
-import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // on below line we are creating variable
+    // for our array list and swipe deck.
+    private SwipeDeck cardStack;
+    private ArrayList<CourseModal> courseModalArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test Connection
-        // FirebaseDatabase.getInstance().getReference().child("Test").setValue("abcd");
-        // Test Connection
+        // on below line we are initializing our array list and swipe deck.
+        courseModalArrayList = new ArrayList<>();
+        cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
 
+        // on below line we are adding data to our array list.
+        courseModalArrayList.add(new CourseModal("C++", "30 days", "20 Tracks", "C++ Self Paced Course", R.drawable.nickistare));
+        courseModalArrayList.add(new CourseModal("Java", "30 days", "20 Tracks", "Java Self Paced Course", R.drawable.uh));
+        courseModalArrayList.add(new CourseModal("Python", "30 days", "20 Tracks", "Python Self Paced Course", R.drawable.patrick));
+        courseModalArrayList.add(new CourseModal("DSA", "30 days", "20 Tracks", "DSA Self Paced Course", R.drawable.zoran));
+
+        // on below line we are creating a variable for our adapter class and passing array list to it.
+        final DeckAdapter adapter = new DeckAdapter(courseModalArrayList, this);
+
+        // on below line we are setting adapter to our card stack.
+        cardStack.setAdapter(adapter);
+
+        // on below line we are setting event callback to our card stack.
+        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
+            @Override
+            public void cardSwipedLeft(int position) {
+                // on card swipe left we are displaying a toast message.
+                Toast.makeText(MainActivity.this, "Card Swiped Left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void cardSwipedRight(int position) {
+                // on card swiped to right we are displaying a toast message.
+                Toast.makeText(MainActivity.this, "Card Swiped Right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void cardsDepleted() {
+                // this method is called when no card is present
+                Toast.makeText(MainActivity.this, "No more courses present", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void cardActionDown() {
+                // this method is called when card is swiped down.
+                Log.i("TAG", "CARDS MOVED DOWN");
+            }
+
+            @Override
+            public void cardActionUp() {
+                // this method is called when card is moved up.
+                Log.i("TAG", "CARDS MOVED UP");
+            }
+        });
     }
 }
